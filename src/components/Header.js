59aac2ccefc141eeeb339dac1c171047793397ee
naiiiden <na4ko359@gmail.com>
2022-05-17 07:38:01 +0300

    add keyframes animation on mobile menu links

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,12 +14,12 @@ const Header = () => {
             </button>
             <nav className="header--nav">
                 <ul className={`header--nav--ul ${!openMenu ? "" : "show"}`}>
-                    <li><a href="" className="header--nav--ul--link">Features</a></li>
-                    <li><a href="" className="header--nav--ul--link">Customer Stories</a></li>
-                    <li><a href="" className="header--nav--ul--link">Pricing</a></li>
-                    <li><a href="" className="header--nav--ul--link">Blog</a></li>
-                    <li><a href="" className="header--nav--ul--link header--nav--ul--invisible">Contact</a></li>
-                    <li><a href="" className="header--nav--ul--link header--nav--ul--invisible">Support</a></li>
+                    <li><a href="" className="header--nav--ul--link link--one">Features</a></li>
+                    <li><a href="" className="header--nav--ul--link link--two">Customer Stories</a></li>
+                    <li><a href="" className="header--nav--ul--link link--three">Pricing</a></li>
+                    <li><a href="" className="header--nav--ul--link link--four">Blog</a></li>
+                    <li><a href="" className="header--nav--ul--link link--five header--nav--ul--invisible">Contact</a></li>
+                    <li><a href="" className="header--nav--ul--link link--six header--nav--ul--invisible">Support</a></li>
                     <li className="header--nav--ul--li--sign-up"><a href="" className="header--nav--ul--sign-up">Sign Up</a></li>
 
                 </ul>
