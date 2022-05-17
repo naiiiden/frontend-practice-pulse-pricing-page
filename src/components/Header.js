02d6c25ffaf8f562fa9bb5c2f0938bc0a7a37e31
naiiiden <na4ko359@gmail.com>
2022-05-17 07:05:10 +0300

    mobile menu now opens/closes properly

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 import Logo from "../images/pulse-logo.svg";
 import Menu from "../images/icon-menu.svg";
+import { useState } from "react";
 
 const Header = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+    const ToggleNav = () => setOpenMenu(!openMenu);
+
     return (
         <header>
             <img src={Logo} alt="Pulse's logo"/>
-            <button aria-label="toggle-navigation" className="header--mobile--menu--toggle">
+            <button aria-label="toggle-navigation" className="header--mobile--menu--toggle" onClick={ToggleNav}>
                 <img src={Menu} alt=""/>
             </button>
             <nav className="header--nav">
-                <ul className="header--nav--ul">
+                <ul className={`header--nav--ul ${!openMenu ? "" : "show"}`}>
                     <li><a href="" className="header--nav--ul--link">Features</a></li>
                     <li><a href="" className="header--nav--ul--link">Customer Stories</a></li>
                     <li><a href="" className="header--nav--ul--link">Pricing</a></li>
