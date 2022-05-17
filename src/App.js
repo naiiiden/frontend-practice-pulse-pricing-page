dddf6eb8397629a67baecd46094e32d6d469bb56
naiiiden <na4ko359@gmail.com>
2022-05-17 14:49:44 +0300

    import & render hero component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./style/general.css";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <div className="App">
       <Header/>
+      <Hero h1Text="Try Pulse free for 30 days." pText="See why Pulse is the best way to monitor your cash flow. Sign up for any plan and try Pulse absolutely free for 30 days."/>
     </div>
   );
 }
