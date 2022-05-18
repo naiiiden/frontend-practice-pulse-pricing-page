123848ba661e9ada10e4876067273063afa54b2c
naiiiden <na4ko359@gmail.com>
2022-05-18 20:44:15 +0300

    basic cocomponent styled on mobile/tablet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./style/general.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Basic from "./components/Basic";
 
 const App = () => {
   return (
     <div className="App">
       <Header/>
       <Hero h1Text="Try Pulse free for 30 days." pText="See why Pulse is the best way to monitor your cash flow. Sign up for any plan and try Pulse absolutely free for 30 days."/>
+      <Basic/>
     </div>
   );
 }
