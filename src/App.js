e801a36652154fc2eaa41697f967084fd5b58a04
naiiiden <na4ko359@gmail.com>
2022-05-18 22:00:13 +0300

    options container styled on mobile/adjust basic container bottom padding

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ const App = () => {
     <div className="App">
       <Header/>
       <Hero h1Text="Try Pulse free for 30 days." pText="See why Pulse is the best way to monitor your cash flow. Sign up for any plan and try Pulse absolutely free for 30 days."/>
-      <Basic/>
-      <Business/>
-      <Features/>
+      <div className="options--container">
+        <Basic/>
+        <Business/>
+        <Features/>
+      </div>
     </div>
   );
 }
