da2d8d0a8b0cc08a314ec54613942bd7d94eb925
naiiiden <na4ko359@gmail.com>
2022-05-18 21:50:41 +0300

    features component styled on mobile

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Basic from "./components/Basic";
 import Business from "./components/Business";
+import Features from "./components/Features";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero h1Text="Try Pulse free for 30 days." pText="See why Pulse is the best way to monitor your cash flow. Sign up for any plan and try Pulse absolutely free for 30 days."/>
       <Basic/>
       <Business/>
+      <Features/>
     </div>
   );
 }
--- src/components/Features.js
+++ src/components/Features.js
@@ -1,16 +1,35 @@
+import checkmark from "../images/icon-check.svg";
+
 const Features = () => {
     return (
         <section role="listitem" className="features--container">
-            <span className="features--span">Complex business with multiple financial accounts, currencies, or auditing needs?</span>
+            <p className="features--complex">Complex business with multiple financial accounts, currencies, or auditing needs?</p>
             <div className="features--subcontainer">
-                <h2 className="features--heading">Unlock Extra Features</h2>
-                <p className="features--price">$89 per month</p>
+                <div className="features--heading--price--container">
+                    <h2 className="features--heading">Unlock Extra Features</h2>
+                    <p className="features--price">$89 per month</p>
+                </div>
                 <a href="" className="features--premium">Try Premium</a>
                 <p className="features--get-all">GET IT ALL, PLUS:</p>
                 <ul className="features--li">
-                    <li>Manage cash flow across unlimited financial accounts</li>
-                    <li>Convert to any currency for localized cash flow reporting and projections</li>
-                    <li>Attach invoices or contracts to your cash flow entries for accountability and auditing</li>
+                    <li>
+                        <div>
+                            <img src={checkmark} alt=""/>
+                            Manage cash flow across unlimited financial accounts
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <img src={checkmark} alt=""/>
+                            Convert to any currency for localized cash flow reporting and projections
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <img src={checkmark} alt=""/>
+                            Attach invoices or contracts to your cash flow entries for accountability and auditing
+                        </div>
+                    </li>
                 </ul>
             </div>
         </section>
