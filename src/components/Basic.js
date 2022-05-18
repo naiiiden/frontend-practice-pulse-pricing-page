709835eafcd78808e1f6f5d839675291cbc8039d
naiiiden <na4ko359@gmail.com>
2022-05-18 06:29:35 +0300

    Pulse Basics component

---
+++ src/components/Basic.js
@@ -0,0 +1,19 @@
+const Basic = () => {
+    return (
+        <section role="listitem" className="pulse--basic--container">
+            <h2 className="pulse--basic--heading">These are what we call the Pulse Basics. They’re what every customer gets.</h2>
+            <ul className="pulse--basic--li">
+                <li>Manage cash flow on a daily, weekly, monthly, or yearly basis</li>
+                <li>Forecast growth with recurring income or expenses that scale automatically</li>
+                <li>Works with any currency</li>
+                <li>See Money In and Money Out, categorize transactions, and run helpful reports</li>
+                <li>Toggle entries and accounts on and off to game out different scenarios</li>
+            </ul>
+            <p className="pulse--basic--paragraph">Just need the Pulse Basics?<br/>
+                <a href="" className="pulse--basic--link">Get them now</a> for $29 per month.
+            </p>
+        </section>
+    )
+}
+
+export default Basic;
