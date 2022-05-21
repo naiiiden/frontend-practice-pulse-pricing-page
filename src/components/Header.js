4e530e966a9a61b4ee6eee90b964ef02cb0d79df
naiiiden <na4ko359@gmail.com>
2022-05-21 10:19:53 +0300

    add missing href in header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,13 +14,13 @@ const Header = () => {
             </button>
             <nav className="header--nav">
                 <ul className={`header--nav--ul ${!openMenu ? "" : "show"}`}>
-                    <li><a href="" className="header--nav--ul--link link--one">Features</a></li>
-                    <li><a href="" className="header--nav--ul--link link--two">Customer Stories</a></li>
-                    <li><a href="" className="header--nav--ul--link link--three">Pricing</a></li>
-                    <li><a href="" className="header--nav--ul--link link--four">Blog</a></li>
-                    <li className="invisible--li"><a href="" className="header--nav--ul--link link--five header--nav--ul--invisible">Contact</a></li>
-                    <li className="invisible--li"><a href="" className="header--nav--ul--link link--six header--nav--ul--invisible">Support</a></li>
-                    <li className="header--nav--ul--li--sign-up"><a href="" className="header--nav--ul--sign-up">Sign Up</a></li>
+                    <li><a href="https://pulseapp.com/features" className="header--nav--ul--link link--one">Features</a></li>
+                    <li><a href="https://pulseapp.com/customer-stories" className="header--nav--ul--link link--two">Customer Stories</a></li>
+                    <li><a href="https://pulseapp.com/pricing" className="header--nav--ul--link link--three">Pricing</a></li>
+                    <li><a href="https://pulseapp.com/blog" className="header--nav--ul--link link--four">Blog</a></li>
+                    <li className="invisible--li"><a href="https://pulseapp.com/contact" className="header--nav--ul--link link--five header--nav--ul--invisible">Contact</a></li>
+                    <li className="invisible--li"><a href="https://help.pulseapp.com" className="header--nav--ul--link link--six header--nav--ul--invisible">Support</a></li>
+                    <li className="header--nav--ul--li--sign-up"><a href="https://signup.pulseapp.com/plus" className="header--nav--ul--sign-up">Sign Up</a></li>
 
                 </ul>
             </nav>
